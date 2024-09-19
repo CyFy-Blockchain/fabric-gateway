288a1d537b05ec879341b6f89f4283600f31ba74
Muhammad Hamza Ashraf <mhamza.141097@gmail.com>
2024-09-19 07:39:39 +0500

    Auth: gRPC setup for Signup call. REST -> gRPC flow.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,6 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { SwaggerModule } from '@nestjs/swagger';
-import { swaggerConfig } from './config/swagger/swagger.config';
+import { initializeApp } from './bootstrap/app.initializer';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  // Set global prefix for all routes
-  app.setGlobalPrefix('api/v1');
-
-  // setting up swagger UI
-  const document = SwaggerModule.createDocument(app, swaggerConfig);
-  SwaggerModule.setup('api', app, document, {
-    jsonDocumentUrl: 'swagger/json',
-  });
-
-  await app.listen(3000);
+  await initializeApp();
 }
 bootstrap();
--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './controllers/auth.controller';
 import { AuthService } from './services/auth.service';
+import { AuthTempController } from './controllers/auth.temp.controller';
 
 @Module({
   imports: [],
-  controllers: [AuthController],
+  controllers: [AuthTempController],
   providers: [AuthService],
 })
 export class AuthModule {}
