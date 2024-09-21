271b98ca4b6a9363db34b1fb5025b390db267386
M Hassan Tariq <mhtariq29@gmail.com>
2024-09-21 19:45:55 +0500

    docs: added js doc for the functions

--- src/modules/auth/helper/fabricWallet.ts
+++ src/modules/auth/helper/fabricWallet.ts
@@ -10,6 +10,10 @@ import { generateUuid } from 'src/utils/uuidGenerator';
 
 export class FabricWallet {
   private static wallet: Wallet;
+
+  /**
+   * Enables the singleton pattern to create a single Wallet instance to be used
+   */
   static async getInstance(walletPath: string) {
     // Create a new  wallet : Note that wallet is for managing identities.
     if (this.wallet) return new FabricWallet();
@@ -28,6 +32,14 @@ export class FabricWallet {
     return new FabricWallet();
   }
 
+  /**
+   * Enables a user to create their certificates using their credentials
+   * @param creds - Body required for the user to generate their certificates
+   * @param creds.username - Username of the user logging in
+   * @param creds.password - Password of the user logging in
+   * @param creds.orgName - Organisation name the user is a part of
+   * @returns @param userUuid - as string containing the unique identifier for the user
+   */
   async loginUser(creds: UserCredentials) {
     const { username, password, orgName } = creds;
     const msp = await fetchMspForOrg(orgName);
@@ -50,6 +62,14 @@ export class FabricWallet {
     return key;
   }
 
+  /**
+   * Enables the admin to signup user in the blockchain
+   * @param creds - Body required to sign up the user
+   * @param creds.adminId - ID of the admin
+   * @param creds.username - Username of the user to be registered
+   * @param creds.orgName - Organisation name of the user to be a part of
+   * @returns @param password - as string containing OTP for the user to login
+   */
   async signupUser(creds: SignUpUserBody) {
     const { username, orgName, affiliation, adminId } = creds;
     const caClient = await buildCAClient(orgName);
@@ -69,6 +89,14 @@ export class FabricWallet {
     return { password: secret };
   }
 
+  /**
+   * Enables the admin to invoke user from the blockchain
+   * @param data - Body required to invoke the user's access
+   * @param data.adminId - ID of the admin
+   * @param data.username - Username of the user to be registered
+   * @param data.orgName - Organisation name of the user to be a part of
+   * @returns @param deleted - as true if the process is successfully completed
+   */
   async removeUser(data: UserRemovalBody) {
     const { username, orgName, adminId } = data;
     const caClient = await buildCAClient(orgName);
--- src/modules/auth/services/auth.service.ts
+++ src/modules/auth/services/auth.service.ts
@@ -9,18 +9,53 @@ import { FabricWallet } from '../helper/fabricWallet';
 const walletPath = path.join(__dirname, 'wallet');
 @Injectable()
 export class AuthService {
+  /**
+   * Authenticates a user by retrieving their UUID from the wallet.
+   *
+   * @param data - The user credentials to authenticate.
+   * @param data.username - The username of the user.
+   * @param data.password - The password of the user.
+   * @param data.orgName - The organisation name user is a part of.
+   *
+   * @returns @param userId - The UUID of the authenticated user.
+   *
+   * @throws Will throw an error if the user credentials are invalid.
+   */
   async loginUser(data: UserCredentials) {
     const wallet = await FabricWallet.getInstance(walletPath);
     const userUuid = await wallet.loginUser(data);
     return { userId: userUuid };
   }
 
+  /**
+   * Registers a new user in the blockchain wallet.
+   *
+   * @param data - The user details to register.
+   * @param data.adminId - The ID of the admin performing the registration.
+   * @param data.username - The username of the new user.
+   * @param data.affiliation - The department MSP for the new user.
+   * @param data.orgName - The name of the organisation user is a part off.
+   *
+   * @returns @param password - The OTP password of the newly created user.
+   */
   async signupUser(data: SignUpUserBody) {
     const wallet = await FabricWallet.getInstance(walletPath);
     const userPassword = await wallet.signupUser(data);
     return userPassword;
   }
 
+  /**
+   * Revokes an existing user's access from the blockchain wallet.
+   *
+   * @param data - The details of the user to be removed.
+   * @param data.adminId - The ID of the admin performing the removal.
+   * @param data.username - The username of the user to be removed.
+   * @param data.orgName - The name of the organisation the user is a part of.
+   *
+   * @returns @param response - The response from the wallet indicating success or failure.
+   *
+   * @throws Will throw an error if the admin credentials are invalid or if the user does not exist.
+   */
   async revokeAccount(data: UserRemovalBody) {
     const wallet = await FabricWallet.getInstance(walletPath);
     const response = await wallet.removeUser(data);
