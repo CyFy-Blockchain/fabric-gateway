f95cc1e87c678519f4baa7ef382e2df1626920d1
Muhammad Hamza Ashraf <mhamza.141097@gmail.com>
2024-09-21 18:51:12 +0500

    Login: Implemented.

--- src/modules/auth/controllers/auth.temp.controller.ts
+++ src/modules/auth/controllers/auth.temp.controller.ts
@@ -1,12 +1,24 @@
 import { Metadata, ServerUnaryCall, status } from '@grpc/grpc-js';
 import { Controller } from '@nestjs/common';
 import { GrpcMethod, RpcException } from '@nestjs/microservices';
-import { EnrollUserCredentials } from '../dto/auth.dto';
+import {
+  EnrollUserCredentials,
+  LoginDto,
+  LoginResponse,
+} from '../dto/auth.dto';
 import { DtoClass } from '../../../common/decorators/dto-class.decorator';
 import { ErrorHandler } from '../../../utils/error-handler.utils';
 
 @Controller()
 export class AuthTempController {
+  /**
+   * Signs up a new user in the system using the provided credentials.
+   * @param data - The user credentials to be used for signup.
+   * @param metadata - Additional metadata to be passed along with the request.
+   * @param call - The gRPC call object used for communication.
+   * @returns A boolean value indicating the success of the signup process.
+   * @throws RpcException - If the signup process encounters an error that is already handled.
+   */
   @GrpcMethod('AuthService', 'Signup')
   @DtoClass(EnrollUserCredentials)
   Signup(
@@ -15,7 +27,6 @@ export class AuthTempController {
     call: ServerUnaryCall<any, any>,
   ): boolean {
     try {
-      console.info('🚀 ~ AuthTempController ~ data:', data);
       return true;
     } catch (error) {
       console.error('🚀 ~ AuthTempController ~ error:', error);
@@ -24,6 +35,35 @@ export class AuthTempController {
         throw error;
       }
       ErrorHandler.handleInternalError('Failed to Signup');
+    }
+  }
+
+  /**
+   * Logs the user into the system using the provided credentials.
+   * @param data - The user credentials to be used for login.
+   * @param metadata - Additional metadata to be passed along with the request.
+   * @param call - The gRPC call object used for communication.
+   * @returns A LoginResponse object containing the user's unique identifier.
+   * @throws RpcException - If the login process encounters an error that is already handled.
+   */
+  @GrpcMethod('AuthService', 'Login')
+  @DtoClass(LoginDto)
+  Login(
+    data: EnrollUserCredentials,
+    metadata: Metadata,
+    call: ServerUnaryCall<any, any>,
+  ): LoginResponse {
+    try {
+      return {
+        userId: '123-456-789',
+      };
+    } catch (error) {
+      console.error('🚀 ~ AuthTempController ~ error:', error);
+      if (error instanceof RpcException) {
+        // Re-throw RpcExceptions if they are already handled
+        throw error;
+      }
+      ErrorHandler.handleInternalError('Failed to Login');
     }
   }
 }
